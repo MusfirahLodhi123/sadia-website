08fb631c3de1ea543b59a88f3949e09ee321e85e
MusfirahLodhi123 <159101447+MusfirahLodhi123@users.noreply.github.com>
2024-05-25 16:40:44 +0500

    Add files via upload

---
+++ tote bags/app.js
@@ -0,0 +1,49 @@
+// const bar = document.getElementById('bar');
+// const nav = document.getElementById('navbar');
+// if(bar){
+//     bar.addEventListener('click',() => {
+//         nav.classList.add('active');
+//     })
+// }
+// if(close){
+//     close.addEventListener('click',() => {
+//         nav.classList.remove('active');
+//     })
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const bar = document.getElementById('bar');
+// const close = document.getElementById('close');
+// const navbar = document.getElementById('navbar');
+
+// if (bar) {
+//     bar.addEventListener('click', () => {
+//         navbar.style.display = 'flex';
+//         close.style.display = 'block';
+//         bar.style.display = 'none';
+//     });
+// }
+
+// if (close) {
+//     close.addEventListener('click', () => {
+//         navbar.style.display = 'none';
+//         close.style.display = 'none';
+//         bar.style.display = 'block';
+//     });
+// }
+// </script>
+
+
+
